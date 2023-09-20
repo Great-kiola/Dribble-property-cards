7330036c79ee5da74a1b0709febe045d4f441339
Ola <hollabodey.kilaso@gmail.com>
2023-09-20 19:47:54 +0100

    Made changes to the css and js file

--- assets/script.js
+++ assets/script.js
@@ -3,87 +3,12 @@ console.log("Hello world!");
 let press = document.querySelectorAll(".press");
 let card = document.getElementById("container");
 
-// console.log(data[0].link)
 
 press.forEach((pressed) => {
   pressed.addEventListener("click", () => {
     pressed.classList.toggle("active");
   });
 });
-
-// fetch("./assets/data.json")
-// .then((res) => res.json())
-// .then((data) => {
-//     console.log(data);
-//     for (const element of data){
-//         try {
-//             card.innerHTML += `
-//             <div class="imgDiv">
-
-//                 <div class="status">
-//                     <p>Active</p>
-//                 </div>
-//                 <img src= ${element[0].link} alt="apartment">
-
-//                 <div class="actions">
-//                     <i class="uil uil-link press"></i>
-//                     <i class="uil uil-heart press"></i>
-//                 </div>
-
-//             </div>
-
-//             <div class="apartDetails">
-//                 <div class="heading">
-//                     <h1>Luxe Apt</h1>
-//                     <h2>USA, New York City</h2>
-//                 </div>
-
-//                 <div class="price">
-//                     <p>Property Price</p>
-//                     <p class="amount">$220,000</p>
-//                 </div>
-//             </div>
-
-//             <div class="iconDiv">
-
-//                 <div class="bed">
-//                     <i class="uil uil-bed-double"></i>
-//                     <span>1</span>
-//                 </div>
-
-//                 <div class="bath">
-//                     <i class="uil uil-bath"></i>
-//                     <span>1</span>
-//                 </div>
-
-//                 <div class="plan">
-//                     <i class="uil uil-vector-square"></i>
-//                     <span>1</span>
-//                 </div>
-
-//                 <div class="car">
-//                     <i class="uil uil-car"></i>
-//                     <span>1</span>
-//                 </div>
-//             </div>
-
-//             <div class="finance">
-//                 <div class="tokenPrice">
-//                     <h2>Token Price</h2>
-//                     <P>$50</P>
-//                 </div>
-//                 <div class="projIRR">
-//                     <h2>Projecte IRR</h2>
-//                     <P>12.3%</P>
-//                 </div>
-//                 <div class="projApr">
-//                     <h2>Projecte APR</h2>
-//                     <P>10.5%</P>
-//                 </div>
-//             </div>`
-//         }
-//     }
-// });
 
 fetch("./assets/data.json")
   .then((res) => res.json())
@@ -124,37 +49,37 @@ fetch("./assets/data.json")
                 
                 <div class="bed">
                     <i class="uil uil-bed-double"></i>
-                    <span>1</span>
+                    <span>${response.bed}</span>
                 </div>
 
                 <div class="bath">
                     <i class="uil uil-bath"></i>
-                    <span>1</span>
+                    <span>${response.bath}</span>
                 </div>
 
                 <div class="plan">
                     <i class="uil uil-vector-square"></i>
-                    <span>1</span>
+                    <span>${response.sqrFt}</span>
                 </div>
 
                 <div class="car">
                     <i class="uil uil-car"></i>
-                    <span>1</span>
+                    <span>0</span>
                 </div>
             </div>
 
             <div class="finance">
                 <div class="tokenPrice">
                     <h2>Token Price</h2>
-                    <P>$50</P>
+                    <P>$${response.tokenPrice}</P>
                 </div>
                 <div class="projIRR">
                     <h2>Projecte IRR</h2>
-                    <P>12.3%</P>
+                    <P>${response.pIRR}%</P>
                 </div>
                 <div class="projApr">
                     <h2>Projecte APR</h2>
-                    <P>10.5%</P>
+                    <P>${response.pAPR}%</P>
                 </div>
             </div>
         </div>
